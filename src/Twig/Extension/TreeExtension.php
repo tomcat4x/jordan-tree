6e3961f6786c03739316ed71cff33fe89b7626b4
tomcat4x <hess.frank@web.de>
2020-11-16 18:36:46 +0100

    Create TreeExtension.php

---
+++ src/Twig/Extension/TreeExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Fuz\Jordan\Twig\Extension;
+
+use Fuz\Jordan\Twig\TokenParser\TreeTokenParser;
+
+class TreeExtension extends \Twig_Extension
+{
+    public function __construct()
+    {
+        if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+            throw new \LogicException('The {%tree%} Twig tag requires PHP version 5.4 or higher');
+        }
+    }
+
+    public function getTokenParsers()
+    {
+        return array(
+            new TreeTokenParser(),
+        );
+    }
+
+    public function getName() {
+        return 'tree';
+    }
+}
